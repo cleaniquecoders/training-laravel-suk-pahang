26232e370aef5f7ea9205f381d08ff011152a790
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-10-24 16:20:00 +0800

    Update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,27 +7,40 @@
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            @can('create-user')
+                <div class="flex justify-end mb-4">
+                    <a href="{{ route('users.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                        Create New User
+                    </a>
+                </div>
+            @endcan
             {{ $users->links() }}
             <div class="my-4">
                 @foreach ($users as $user)
                     <div class="p-4 shadow-md rounder-md flex justify-between bg-white">
-                        {{ $user->name }}
+                        <div>
+                            {{ $user->name }} - {{ $user->email }}
+                        </div>
 
                         <div class="flex gap-4">
-                            <a class="hover:underline hover:text-blue-500" href="{{ route('users.show', $user) }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                </svg>
-                            </a>
-                            <a class="hover:underline hover:text-blue-500" href="{{ route('users.edit', $user) }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                                </svg>
-                            </a>
+                            @can('view', $user)
+                                <a class="hover:underline hover:text-blue-500" href="{{ route('users.show', $user) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                    </svg>
+                                </a>
+                            @endcan
+                            @can('update', $user)
+                                <a class="hover:underline hover:text-blue-500" href="{{ route('users.edit', $user) }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                    </svg>
+                                </a>
+                            @endcan
                             @can('delete', $user)
                                 <div>
                                     <form action="" class="hover:underline hover:text-red-500 cursor-pointer">
