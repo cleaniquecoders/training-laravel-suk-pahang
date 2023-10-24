63d8ccdbb0a9bc59029379350de37ed6e57d8a56
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-10-24 09:07:49 +0800

    Added mail

---
+++ resources/views/mail/quiz-reminder.blade.php
@@ -0,0 +1,8 @@
+<x-mail::message>
+We noticed you haven't completed the quiz form yet. It's not too late! Your insights are valuable to us.
+
+Thank you for your participation!
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
